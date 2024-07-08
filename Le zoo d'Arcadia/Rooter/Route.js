7779280f65fc393b6946749982a3a43ea4abfe87
Antan-gi <Ibanezguillaume85@gmail.com>
2024-07-08 14:02:32 +0200

    restart

--- Le zoo d'Arcadia/Rooter/Route.js
+++ Le zoo d'Arcadia/Rooter/Route.js
@@ -1,8 +1,9 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, pathJS, pages = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.page = pages;
     }
 }
